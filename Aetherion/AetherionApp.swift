c7e05c273ddfc8d13f775e7eac9e7184a071a287
K-Cim <k-cim@aetherion.net>
2025-08-28 21:18:13 +0200

    Initial Commit

---
+++ Aetherion/AetherionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AetherionApp.swift
+//  Aetherion
+//
+//  Created by Mickael Puaud on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AetherionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
